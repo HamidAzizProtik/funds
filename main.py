e390478e46a28938122fb949fdf4642d8cb83b61
Hamid Aziz Protik <protikhamidaziz@gmail.com>
2026-06-13 23:03:00 +0600

    adding a banner, functions for modular code and strengthening logic

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-monthly = float(input("enter monthly revenue: "))
+def get_float(prompt):
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("please enter a number")
+
+print(r'''
+┏┓ ╻ ╻╺┳┓┏━╸┏━╸╺┳╸╻┏┓╻┏━╸   ┏━┓┏━┓┏━┓
+┣┻┓┃ ┃ ┃┃┃╺┓┣╸  ┃ ┃┃┗┫┃╺┓   ┣━┫┣━┛┣━┛
+┗━┛┗━┛╺┻┛┗━┛┗━╸ ╹ ╹╹ ╹┗━┛   ╹ ╹╹  ╹  
+''')
+
+monthly = get_float("enter monthly revenue: ")
 spent = 0
 expenses = []
+
 while True:
     name = input("enter name of the expense (done = stop): ")
+
     if name == "done":
         print(f"you spent {spent:.2f} and have {monthly - spent:.2f} left behind")
         print(expenses)
-
         totals = {}
+
         for e in expenses:
             option_name = e["name"]
             amount = e["amount"]
+
             if option_name in totals:
                 totals[option_name] += amount
             else:
@@ -24,7 +40,8 @@ while True:
         else:
             print("you spent too much")
         break
+
     else:
-        cost = float(input("enter cost of expense: "))
+        cost = get_float("enter cost of expense: ")
         spent = spent + cost
         expenses.append({"name": name, "amount": cost})
